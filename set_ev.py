ad2a666ba36d75317329776fc4e1b45936478c00
zjwall <zjwall@gmail.com>
2023-06-29 10:10:45 -0700

    Barium Experiment 2 Backup
    New code from new experiment including artiq and python3 labrad updates

---
+++ set_ev.py
@@ -0,0 +1,13 @@
+import os
+
+os.environ['LABRAD_TLS'] = 'off'
+
+os.environ['LABRAD_TLS_PORT'] = '7643'
+
+os.environ['LABRADNODE'] = 'DESKTOP-57250L5'
+
+os.environ['LABRAD_HOST'] = 'localhost'
+
+os.environ['LABRADPASSWORD'] = 'lab'
+
+os.environ['LABRADPORT'] = '7682'
---
+++ startup/2)start_web_client.bat
@@ -0,0 +1,2 @@
+cd C:\Users\barium133\Code\scalabrad-web-server-2.0.6\bin
+labrad-web
